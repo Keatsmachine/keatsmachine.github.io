342d0e5307bdd9b49cceed13e2a091981f8bc639
Darren <Darren.j.keats@gmail.com>
2022-08-30 20:51:31 +0100

    Swap to hash router for GH pages

--- keatsmachine-github-pages/src/index.js
+++ keatsmachine-github-pages/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
 
 import AboutMe from './pages/AboutMe';
@@ -11,17 +11,17 @@ import TravelersParadoxPrivacy from "./pages/TravelersParadoxPrivacy";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="Home" element={<Home />} />
         <Route path="About" element={<AboutMe />} />
         <Route path="TravelersParadox" element={<TravelersParadox />} />
         <Route path="TP_Privacy" element={<TravelersParadoxPrivacy />} />
-        <Route path="*" element={<BlankPage/>} />
+        <Route path="*" element={<BlankPage />} />
         {/* <Route path="*" element={<p>There is nothing here</p>} /> */}
         <Route path="" element={<Home />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
